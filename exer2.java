eac1e15c91222ba37432df10f702b3d8e390c259
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-09-22 18:04:33 -0400

    ev

---
+++ exer2.java
@@ -0,0 +1,15 @@
+// Create a class that has as a data member a 2D array of ints, and an int member for the dimension.
+// The class constructor should take an integer, n, and create and instantiate the array with 0s in each “cell”. (Save “n” in the “dimension” data variable.
+// In the constructor, catch Array Index out of bounds.
+// In the client (main), create an instance of the class. Surround it in a try-catch block that checks for mathematical errors (ArithmeticException) and also generic exceptions (2 catches).
+// Use a finally block to print out the dimensions of the array (nicely - don’t just print out n.)
+// Test your code out to make sure your exceptions are caught.
+
+public class exer2 {
+    public static void main(String[] args) {
+        
+    }
+
+
+
+}
