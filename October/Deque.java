88f61f1c4ac5495a942330ba1ce827f0419a894c
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-10-25 14:24:23 -0400

    ev

--- October/Deque.java
+++ October/Deque.java
@@ -17,6 +17,29 @@ public class Deque < Item > implements Iterable < Item > {
         size = 0;
     }
 
+    public Iterator < Item > iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator < Item > {
+        private Node current = first;
+
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public remove() }{
+
+        }
+    }
+
     // is the deque empty?
     public boolean isEmpty() {
 
@@ -29,17 +52,24 @@ public class Deque < Item > implements Iterable < Item > {
 
     // add the item to the front
     public void addFirst(Item item) {
-        current.next = current.next.next;
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
     }
 
     // add the item to the back
     public void addLast(Item item) {
+        if(size == 0) {
+            first = newNode();
+            
 
+        }
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
-
+        
     }
 
     // remove and return the item from the back
@@ -48,9 +78,6 @@ public class Deque < Item > implements Iterable < Item > {
     }
 
     // return an iterator over items in order from front to back
-    public Iterator < Item > iterator() {
-
-    }
 
     // unit testing (required)
     public static void main(String[] args) {
