22716b52de7c7f2067d5c8c0185b227c2293e65a
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-11-16 12:27:40 -0500

    ev

---
+++ Main2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        int gradeCount = 0;
+        Double sum = 0.0;
+        SymbolTable < String, Double > grades = new SymbolTable < String, Double > ();
+        grades.put("A+", 4.33);
+        grades.put("A", 4.00);
+        grades.put("A-", 3.67);
+        grades.put("B+", 3.33);
+        grades.put("B", 3.00);
+        grades.put("B-", 2.67);
+        grades.put("C+", 2.33);
+        grades.put("C", 2.00);
+        grades.put("C-", 1.67);
+        grades.put("D", 1.00);
+        grades.put("F", 0.00);
+        Scanner scanner = new Scanner(System.in);
+        //Use ^D for EOF (end of file)
+
+        while (scanner.hasNext()) {
+            String key = scanner.next();
+            System.out.println(key + "!");
+            Double value = grades.get(key);
+            if (value != null) {
+                sum = sum + value;
+                gradeCount++;
+            } else {
+                System.out.println(key + " is an invalid grade");
+            }
+        }
+        scanner.close();
+
+
+        System.out.println("GPA :" + sum / gradeCount);
+
+        for (String s: grades) {
+            System.out.println(s);
+        }
+    }
+
+}
---
+++ SymbolTable.java
@@ -0,0 +1,60 @@
+import java.util.Iterator;
+
+public class SymbolTable < Key, Value > implements Iterable < Key> {
+    private Node first;
+    private int N;
+
+    public SymbolTable() {
+
+    }
+
+    private class Node {
+        Key key;
+        Value value;
+        Node next;
+    }
+
+
+    public void put(String key, Double value) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.value = value;
+                return;
+            }
+        }
+        first = new Node(key, value, first);
+        N++;
+    }
+
+    public void get(Key key) {
+        if(key == null)  {
+
+        }
+    }
+
+    public void delete(Key key) {
+
+    }
+
+    public boolean contains(Key key) {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    @Override
+    public Iterator<Key> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+
+
+}
